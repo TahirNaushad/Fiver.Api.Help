b9e98a6c5d7e00ba0ad812619a9febf0ca032544
Tahir Naushasd <t.naushad25@gmail.com>
2017-07-11 10:58:59 +0100

    swagger demo

---
+++ Fiver.Api.Help/Controllers/MoviesController.cs
@@ -0,0 +1,51 @@
+﻿using Fiver.Api.Help.Models.Movies;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Fiver.Api.Help.Controllers
+{
+    [Route("movies")]
+    public class MoviesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var outputModel = new List<MovieOutputModel>();
+            return Ok(outputModel);
+        }
+
+        [HttpGet("{id}", Name = "GetMovie")]
+        public IActionResult Get(int id)
+        {
+            var outputModel = new MovieOutputModel();
+            return Ok(outputModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]MovieInputModel inputModel)
+        {
+            var outputModel = new MovieOutputModel();
+            return CreatedAtRoute("GetMovie", new { id = outputModel.Id }, outputModel);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody]MovieInputModel inputModel)
+        {
+            return NoContent();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult UpdatePatch(
+            int id, [FromBody]JsonPatchDocument<MovieInputModel> patch)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
--- Fiver.Api.Help/Startup.cs
+++ Fiver.Api.Help/Startup.cs
@@ -1,37 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Fiver.Api.Help
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(
+            IServiceCollection services)
         {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("help", new Info
+                {
+                    Title = "Fiver.Api Help",
+                    Version = "v1"
+                });
+            });
+
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+                options.SwaggerEndpoint("/swagger/help/swagger.json", "Fiver.Api Help Endpoint");
             });
+
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
